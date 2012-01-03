2dc7ad13ac3aa14a66328587edbc33796f317deb
pifta <istvan.pentek@gmail.com>
2012-01-03 17:47:34 +0100

    The project template and the first logging application.

---
+++ MicrosoftEnterpriseLibraryLogging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+namespace MicrosoftEnterpriseLibraryLogging
+{
+    class Program
+    {
+        private static LogWriter writer = EnterpriseLibraryContainer.Current.GetInstance<LogWriter>();
+
+        static void Main(string[] args)
+        {
+            LogEntry entry = new LogEntry();
+
+            entry.Message = "Application started.";
+            entry.Categories.Add("LogCategory");
+
+            writer.Write(entry);
+        }
+    }
+}
